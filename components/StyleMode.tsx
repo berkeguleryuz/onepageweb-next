3949da7250b541bd9a0851417da22ac9e2b1c3e9
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-08-24 18:22:44 +0200

    update

--- components/StyleMode.tsx
+++ components/StyleMode.tsx
@@ -20,13 +20,13 @@ const StyleMode = (props: Props) => {
     <section className="py-16">
       <div className="overflow-x-clip bg-gradient-to-r from-orange-300 to-lime-400 -rotate-3">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex py-3 items-center flex-none gap-4">
+          <div className="flex py-3 items-center flex-none gap-4 md:gap-16">
             {words.map((word) => (
               <div key={word} className="inline-flex gap-4">
                 <span className="text-neutral-950 uppercase font-extrabold text-sm">
                   {word}
                 </span>
-                <PiStarFourFill className="size-5 text-neutral-950" />
+                <PiStarFourFill className="size-5 -rotate-12 text-neutral-950" />
               </div>
             ))}
           </div>
